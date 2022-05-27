f65cb8e79dec0bb0d9d4d40f6c9e5061473f5b29
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-05-27 22:04:18 +0300

    first commit

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -3,7 +3,7 @@ import s from './Post.module.css'
 import {message} from "antd";
 
 
-function Post (props:message) {
+function Post (props) {
     return (
         <div className={s.item}>
             <img src={'https://mport.ua/i/11/51/76/9/1151769/image_main/e4b03950eccc4611f57d4950fd41e972-quality_70Xresize_crop_1Xallow_enlarge_0Xw_1200Xh_643.jpg'}/>

c0cb15ccea820878dd0dcc7cc9cd14179c916838
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-01 13:31:46 +0300

    71SetStateStatusInputCommit

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-export type ProfileStatusPropsType={
-    status:string
+
+export type ProfileStatusPropsType = {
+    status: string
 }
 
 class ProfileStatus extends React.Component<any, any> {
-state={
-    editMode:false
+    state = {
+        editMode: false
+    }
+activateEditMode=()=>{
+        this.setState({
+            editMode:true
+        })
+
+
+
 }
     render() {
         return (
-            <div>{!this.state.editMode && <div><span>{this.props.status}</span></div>
+            <div>{!this.state.editMode && <div><span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span></div>
             }
-                {this.state.editMode &&<div><input value={this.props.status}/></div>
+                {this.state.editMode && <div><input value={this.props.status}/></div>
                 }
 
             </div>

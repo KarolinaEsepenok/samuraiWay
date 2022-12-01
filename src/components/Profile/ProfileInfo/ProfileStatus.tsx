d6fcebd9495154e5e83a4d6d86fb6b2aaa212d84
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-01 18:32:36 +0300

    71SetStateStatusInputCommit

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -12,15 +12,17 @@ activateEditMode=()=>{
         this.setState({
             editMode:true
         })
-
-
-
 }
+    deactivateEditMode=()=>{
+        this.setState({
+            editMode:false
+        })
+    }
     render() {
         return (
             <div>{!this.state.editMode && <div><span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span></div>
             }
-                {this.state.editMode && <div><input value={this.props.status}/></div>
+                {this.state.editMode && <div><input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/></div>
                 }
 
             </div>

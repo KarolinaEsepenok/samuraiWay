f1f3681fa6dee894f3c05b9fe2309ba36315fbaa
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-08 13:00:58 +0300

    75LoginFormCommit

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            LOGIN
+            <h2>LOGIN</h2>
+            <form>
+                <div><input placeholder={'Login'}/></div>
+                <div><input placeholder={'Password'}/></div>
+                <div><input type={'checkbox'}/>Remember me</div>
+                <div><button>Login</button></div>
+            </form>
         </div>
     );
 };

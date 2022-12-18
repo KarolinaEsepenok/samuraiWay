bc9bcabd6c71d1a2f4955770f51efa39c98f337a
karolinasmile@mail.ru <karolinasmile@mail.ru>
2022-12-18 16:10:37 +0300

    80loginLogoutgoodCss

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,21 +6,21 @@ import {NavLink} from "react-router-dom";
 function Navbar() {
     return (
         <nav className={s.nav}>
-            <div className={s.items}>
-            <div className={s.item}>
-                <NavLink to={"/profile"} activeClassName={s.active}>Profile</NavLink></div>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to={"/dialogs"} activeClassName={s.active}>Messages</NavLink></div>
-            <div className={s.item}>
-                <NavLink to={"/news"} activeClassName={s.active}>News</NavLink></div>
-            <div className={s.item}>
-                <NavLink to={"/music"} activeClassName={s.active}>Music</NavLink></div>
+            <ul className={s.items}>
+            <li className={s.item}>
+                <NavLink to={"/profile"} activeClassName={s.active}>Profile</NavLink></li>
+            <li className={`${s.item} ${s.active}`}>
+                <NavLink to={"/dialogs"} activeClassName={s.active}>Messages</NavLink></li>
+            <li className={s.item}>
+                <NavLink to={"/news"} activeClassName={s.active}>News</NavLink></li>
+            <li className={s.item}>
+                <NavLink to={"/music"} activeClassName={s.active}>Music</NavLink></li>
 
-            <div className={s.item}>
-                <NavLink to={"/settings"} activeClassName={s.active}>Settings</NavLink></div>
-            <div className={s.item}>
-                <NavLink to={"/users"} activeClassName={s.active}>Users</NavLink></div>
-        </div>
+            <li className={s.item}>
+                <NavLink to={"/settings"} activeClassName={s.active}>Settings</NavLink></li>
+            <li className={s.item}>
+                <NavLink to={"/users"} activeClassName={s.active}>Users</NavLink></li>
+        </ul>
         </nav>
     )
 }
